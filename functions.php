edf72690f672c00f8b40289f9d308b797a6fc289
Jeremy Marchandeau <jeremy@tigerandjune.com>
2021-04-07 22:35:33 +0200

    wrong name in load wpfb function

--- functions.php
+++ functions.php
@@ -113,11 +113,11 @@ class StarterSite extends Timber\Site {
 		*  Load WP Fizz Builder
 		*/
 
-		   // Define path and URL to the ACF plugin.
+		   // Define path and URL to the WPFB plugin.
 		   define( 'WPFB_PATH', get_stylesheet_directory() . '/vendor/jeremy6680/wp-fizz-builder/' );
 		   define( 'WPFB_URL', get_stylesheet_directory_uri() . '/vendor/jeremy6680/wp-fizz-builder/' );
 
-		   // Include the ACF plugin.
+		   // Include the WPFB plugin.
 		   include_once( WPFB_PATH . 'index.php' );
 		   $wpfb    = new WPFB();
 		   $wpfb->init();
